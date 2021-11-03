db11fa4af290da611f3b2f6350e9bfd0b25e7e53
Abhishek <abhishekyadavfeb1498@gmail.com>
2021-11-03 22:36:45 +0530

    searchBar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import './NavBar.css';
-import { BsHouseDoor,BsPeopleFill,BsFillChatRightTextFill } from "react-icons/bs"
+import { BsHouseDoor,BsPeopleFill,BsFillChatRightTextFill } from "react-icons/bs";
+import {BiSearchAlt2} from 'react-icons/bi'
 
 const NavBar = () => {
+  const [searchInput,setSearchInput]=useState();
+  const searchInputValue =(event)=>{
+ console.log(event.target.value);
+  }
   return (
     <div className='main_navbar_div'>
       <div className='left_div'>
         <h1>Social Media</h1>
         <div className='search_bar_div'>
-          <input placeholder='search' type='text' />
+				<BiSearchAlt2/>
+          <input placeholder='search' type='text'  onChange={searchInputValue} />
         </div>
       </div>
       <div className='right_div'>
